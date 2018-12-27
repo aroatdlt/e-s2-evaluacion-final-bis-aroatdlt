2ef2d27a8fcaf312511f9c6c188ef22cdd243adb
aroatdlt <aroatato@gmx.com>
2018-12-27 13:57:24 +0100

    Add function handle to button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,6 +1,19 @@
 'use strict';
 
 //Recoger el valor del input seleccionado 
+const button = document.querySelector('.button');
+
+function handleBeginGame(){
+    const inputs = document.querySelectorAll('input');
+    const selectedInput = "";
+    for (const input of inputs){
+        
+    }
+
+}
+
+
+button.addEventListener('click', handleBeginGame)
 
 
 //Conectarse a la API
