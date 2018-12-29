9bff55e939920ce0fb77c68b1f377a3f9fda8966
aroatdlt <aroatato@gmx.com>
2018-12-29 17:04:48 +0100

    Add petition to server in js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,18 +2,34 @@
 
 //Recoger el valor del input seleccionado 
 const button = document.querySelector('.button');
+const cardList = document.querySelector('.card__list');
+let imageCard = "";
 
 function handleBeginGame(){
     const inputs = document.querySelectorAll('input');
-    const selectedInput = "";
+    let selectedInput = "";
     for (const input of inputs){
-        
+        if (input.checked){
+            selectedInput = input.value;
+        }
     }
+    fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${selectedInput}.json`)
+    .then(response => response.json())
+    .then(imageData => {
+        for (let i = 0; i < imageData.length; i++){
+            imageCard = imageData[i].image;
+            const newCard = document.createElement('li');
+            const newImage = document.createElement('img');
+            newImage.src = `${imageCard}`;
+            newImage.setAttribute('alt', 'Carta Pokemon');
+            newCard.appendChild(newImage);
+            cardList.appendChild(newCard);
+        }
+    })
+};
 
-}
 
-
-button.addEventListener('click', handleBeginGame)
+button.addEventListener('click', handleBeginGame);
 
 
 //Conectarse a la API
