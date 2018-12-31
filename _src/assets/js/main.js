fee3a9ff50e7a85543c107c3a3b5ed9bcf1df03b
aroatdlt <aroatato@gmx.com>
2018-12-31 14:17:23 +0100

    Add comments in code and indent all code

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//Recoger el valor del input seleccionado 
 const button = document.querySelector('.button');
 const cardList = document.querySelector('.card__list');
 const defaultInput = document.querySelector('#number__four');
@@ -9,67 +8,73 @@ let imageCard = "";
 let selectedInput = "";
 let lastSelectedInput;
 
+//Checked inputs default and the last one checked
 if (localStorage.getItem('numberCards')) {
-    lastSelectedInput = localStorage.getItem('numberCards');
-    for (const input of inputs) {
-        if (lastSelectedInput === input.value) {
-            input.setAttribute('checked', true);
-        }
+  lastSelectedInput = localStorage.getItem('numberCards');
+  for (const input of inputs) {
+    if (lastSelectedInput === input.value) {
+      input.setAttribute('checked', true);
     }
+  }
 } else {
-    defaultInput.setAttribute('checked', true);
+  defaultInput.setAttribute('checked', true);
 }
 
+//Delete all the contain when you choose another number of cards
 function resetGame() {
-    const cardItems = document.querySelectorAll('.card__image');
-    for (const item of cardItems) {
-        item.remove(item);
-    }
+  const cardItems = document.querySelectorAll('.card__image');
+  for (const item of cardItems) {
+    item.remove(item);
+  }
 }
 
+//Begin the game when you choose one option of number of cards
 function handleBeginGame() {
-    resetGame();
-    const inputs = document.querySelectorAll('input');
+  resetGame();
+  const inputs = document.querySelectorAll('input');
 
-
-    for (const input of inputs) {
-        if (input.checked) {
-            selectedInput = input.value;
-        }
+  //Pick up input value
+  for (const input of inputs) {
+    if (input.checked) {
+      selectedInput = input.value;
     }
+  }
 
-    //Pedir al servidor el número de cartas que queremos jugar
-    fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${selectedInput}.json`)
-        .then(response => response.json())
-        .then(imageData => {
-            for (let i = 0; i < imageData.length; i++) {
-                imageCard = imageData[i].image;
-                const newCard = document.createElement('li');
-                newCard.className = 'card__image';
-                const newImage = document.createElement('img');
-                newImage.src = `${imageCard}`;
-                newImage.setAttribute('alt', 'Carta delantera Pokemon');
-                newImage.className = 'pokemon__card--forward';
-                const newImageDefault = document.createElement('img');
-                newImageDefault.src = `https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB.`;
-                newImageDefault.setAttribute('alt', 'Carta trasera Adalab');
-                newImageDefault.className = 'pokemon__card--reverse';
-                newCard.append(newImage, newImageDefault);
-                cardList.appendChild(newCard);
-            }
+  //Ask to server the number of cards which you want to play and print in html
+  fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${selectedInput}.json`)
+    .then(response => response.json())
+    .then(imageData => {
+      for (let i = 0; i < imageData.length; i++) {
+        imageCard = imageData[i].image;
+        const newCard = document.createElement('li');
+        newCard.className = 'card__image';
+        const newImage = document.createElement('img');
+        newImage.src = `${imageCard}`;
+        newImage.setAttribute('alt', 'Carta delantera Pokemon');
+        newImage.className = 'pokemon__card--forward';
+        const newImageDefault = document.createElement('img');
+        newImageDefault.src = `https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB.`;
+        newImageDefault.setAttribute('alt', 'Carta trasera Adalab');
+        newImageDefault.className = 'pokemon__card--reverse';
+        newCard.append(newImage, newImageDefault);
+        cardList.appendChild(newCard);
+      }
 
-            const reverseCards = document.querySelectorAll('.pokemon__card--reverse');
-            for (let i = 0; i < reverseCards.length; i++){
-                reverseCards[i].addEventListener('click', handleSelectedCard);
-            }
-        })
+      //Add listeners to all printed reverse cards
+      const reverseCards = document.querySelectorAll('.pokemon__card--reverse');
+      for (let i = 0; i < reverseCards.length; i++) {
+        reverseCards[i].addEventListener('click', handleSelectedCard);
+      }
+    })
 
-    localStorage.setItem('numberCards', (selectedInput));
+  //Storage the number of cards that you play
+  localStorage.setItem('numberCards', (selectedInput));
 
-    function handleSelectedCard(event) {
-        const selectedCard = event.target;
-        selectedCard.classList.toggle('hidden');
-    }
+  //When you click one card, it will be disappear and we can see pokemon card
+  function handleSelectedCard(event) {
+    const selectedCard = event.target;
+    selectedCard.classList.toggle('hidden');
+  }
 };
 
 button.addEventListener('click', handleBeginGame);
